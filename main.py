3bba9461c7f6c611502bc0ffd26d67be9bce8049
Brendan Gilroy <bdgilroy@gmail.com>
2023-07-19 08:20:22 -0400

    more of a game loop

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
 import random
 import math
 
+first_number_choice = None
+second_number_choice = None
 digits = []
 operations = ['+', '-', '*', r'/']
+unique_switch = False
 
 for i in range(6):
-    digits.append(math.floor(random.random()*25))
+    while not unique_switch:
+        candidate = math.floor(random.random()*25)
+        if candidate not in digits:
+            digits.append(candidate)
+        unique_switch = True
+    unique_switch = False
 
-print("These are your numbers")
-print("    ".join(f'{num}' for num in digits))
+while first_number_choice != 'x':
+    print("These are your numbers")
+    print("    ".join(f'{num}' for num in digits))
 
-print()
+    print()
 
-print("These are your operations")
-print("     ".join(f'{op}' for op in operations))
+    print("These are your operations")
+    print("     ".join(f'{op}' for op in operations))
 
-first_number_choice = input("Choose a first number ")
-second_number_choice = input("Choose a second number ")
+    print()
+    print('Press "x" to quit')
+    while not first_number_choice:
+        try:
+            first_number_choice = input("Choose a first number ")
+            if first_number_choice == 'x':
+                break
+            first_number_choice = int(first_number_choice)
+            digits.remove(first_number_choice)
+        except ValueError:
+            print("Number not in list")
+            first_number_choice = None
+            continue
+    if first_number_choice == 'x':
+        break
 
-operation_choice = input("Choose an operation ")
+    while not second_number_choice:
+        try:
+            second_number_choice = input("Choose a second number ")
+            if second_number_choice == 'x':
+                break
+            second_number_choice = int(second_number_choice)
+            digits.remove(second_number_choice)
+        except ValueError:
+            print("Number not in list")
+            second_number_choice = None
+            continue
+    if second_number_choice == 'x':
+        break
 
+    operation_choice = input("Choose an operation ")
+    if operation_choice == 'x':
+        break
+
+    if operation_choice == '+':
+        digits.append(first_number_choice + second_number_choice)
+    elif operation_choice == '-':
+        digits.append(first_number_choice - second_number_choice)
+    elif operation_choice == '*':
+        digits.append(first_number_choice * second_number_choice)
+    elif operation_choice == r'/':
+        digits.append(first_number_choice / second_number_choice)
+
+    first_number_choice = None
+    second_number_choice = None
