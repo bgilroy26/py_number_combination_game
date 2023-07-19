72f835abcbe8060ce9d61e2f8c59d3ef1b2645d1
Brendan Gilroy <bdgilroy@gmail.com>
2023-07-19 10:10:18 -0400

    almost working; need to ensure all digits are whole numbers and that target is not float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import math
+import itertools
 
 first_number_choice = None
 second_number_choice = None
@@ -7,7 +8,86 @@ digits = []
 operations = ['+', '-', '*', r'/']
 unique_switch = False
 
-for i in range(6):
+
+def evaluate_expression(first, second, op):
+
+    output = None
+
+    if op == '+':
+        output = first + second
+    elif op == '-':
+        output = first - second
+    elif op == '*':
+        output = first * second
+    elif op == r'/':
+        output = first / second
+
+    return output
+
+
+# numbers is a list of 6 numbers that we
+# have at the beginning of the game
+def create_target(numbers):
+    save = numbers
+    # generate the number of numbers that you will use (at least 3)
+    numbers_used = math.floor(random.random()*3)+3
+    operations_used = numbers_used - 1
+
+    pre_target_numbers = random.choices(digits, k=numbers_used)
+    pre_target_operations = random.choices(operations, k=operations_used)
+
+    combo = itertools.zip_longest(pre_target_numbers, pre_target_operations)
+
+    # flatten combo and return list of numbers and operations
+    return ([item for sublist in combo for item in sublist], numbers)
+
+
+
+#expr is the numbers and operations from create_target
+def evaluate_target(expr_pair):
+    expr = expr_pair[0]
+    save = expr_pair[1]
+    # remove None from tail
+    expr.pop()
+
+    len_switch = True
+    positive_switch = True
+    integer_switch = True
+    abs_switch = True
+    while len_switch or positive_switch or integer_switch or abs_switch:
+        first_number = expr.pop()
+        operation = expr.pop()
+        second_number = expr.pop()
+        expr.append(evaluate_expression(first_number,
+                                        second_number,
+                                        operation
+                                        )
+                    )
+
+        if len(expr) == 1:
+            len_switch = False
+        if not len_switch:
+            if expr[0] < 400:
+                abs_switch = False
+            else:
+                return evaluate_target(create_target(save))
+        if not abs_switch:
+            if expr[0] > 0:
+                positive_switch = False
+            else:
+                return evaluate_target(create_target(save))
+        if not positive_switch:
+            if expr[0] == int(expr[0]):
+                integer_switch = False
+            else:
+                return evaluate_target(create_target(save))
+
+    return expr
+
+##############
+# BEGIN GAME #
+##############
+for _ in range(6):
     while not unique_switch:
         candidate = math.floor(random.random()*25)
         if candidate not in digits:
@@ -15,7 +95,11 @@ for i in range(6):
         unique_switch = True
     unique_switch = False
 
+target = evaluate_target(create_target(digits))
+
 while first_number_choice != 'x':
+    print(f"This is your target: {target}")
+    print()
     print("These are your numbers")
     print("    ".join(f'{num}' for num in digits))
 
@@ -58,14 +142,16 @@ while first_number_choice != 'x':
     if operation_choice == 'x':
         break
 
-    if operation_choice == '+':
-        digits.append(first_number_choice + second_number_choice)
-    elif operation_choice == '-':
-        digits.append(first_number_choice - second_number_choice)
-    elif operation_choice == '*':
-        digits.append(first_number_choice * second_number_choice)
-    elif operation_choice == r'/':
-        digits.append(first_number_choice / second_number_choice)
+    new_value = evaluate_expression(first_number_choice,
+                                      second_number_choice,
+                                      operation_choice
+                                      )
+
+    if new_value == target:
+        print("YOU WIN, YOU MARVELOUS DEVIL!")
+        break
+    else:
+        digits.append(new_value)
 
     first_number_choice = None
     second_number_choice = None
